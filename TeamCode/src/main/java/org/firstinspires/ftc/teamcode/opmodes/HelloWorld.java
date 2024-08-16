088f41ce9c9c402ab7f6ec73d97131e6e32e283e
Calvin Tallent <calvintallentspam@gmail.com>
2024-08-15 19:10:52 -0700

    Add a basic function to snap to a cardinal direction.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/HelloWorld.java
@@ -4,6 +4,7 @@ package org.firstinspires.ftc.teamcode.opmodes;
 import android.drm.DrmStore;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.DualNum;
@@ -31,6 +32,7 @@ import java.util.List;
  *
  */
 @TeleOp
+@Config
 public class HelloWorld extends LinearOpMode {
 
    private FtcDashboard dash = FtcDashboard.getInstance();
@@ -41,6 +43,16 @@ public class HelloWorld extends LinearOpMode {
 //    private DcMotor rightFront;
 //    private DcMotor rightBack;
 
+
+
+    double sigmoid(double x){
+        if(x == 0) {
+            return 0;
+        }
+        double sign = x / Math.abs(x); // will be either +1 or -1
+
+        return sign * (1.0 / (1 + Math.pow(Math.E, -(10*Math.abs(x) - 5))));
+    }
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -65,14 +77,37 @@ public class HelloWorld extends LinearOpMode {
         while(opModeIsActive()){
             TelemetryPacket packet = new TelemetryPacket();
 
+            drive.updatePoseEstimate();
+
 
             double y = -gamepad1.left_stick_y;
             double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
 
 
+            double newY = sigmoid(y);
+            double newX = sigmoid(x);
+            double newRx = sigmoid(rx);
 
-            drive.teleOp(y * 10,x * 10,rx);
+
+            PoseVelocity2d input = new PoseVelocity2d(new Vector2d(newY,-newX),-newRx);
+            if(gamepad1.dpad_up){
+                input = snapToRotation(input, drive.pose, 0);
+            }else if(gamepad1.dpad_right){
+                input = snapToRotation(input, drive.pose, -Math.PI/2);
+            }else if(gamepad1.dpad_down){
+                input = snapToRotation(input, drive.pose, Math.PI);
+            }else if(gamepad1.dpad_left){
+                input = snapToRotation(input, drive.pose, Math.PI/2);
+            }
+//            if(gamepad1.right_bumper){
+//                input = snapToRotation(input, drive.pose);
+//            }
+
+
+            drive.setDrivePowers( input );
+
+            //drive.teleOp(y * 10,x * 10,rx);
 
 
 //            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
@@ -101,5 +136,15 @@ public class HelloWorld extends LinearOpMode {
         }
 
 
+    }
+
+
+    public static double P = 0.5;
+    PoseVelocity2d snapToRotation(PoseVelocity2d input, Pose2d currentPose, double desiredDirection){
+        Pose2d target = new Pose2d(currentPose.position, desiredDirection);
+
+        double error = target.heading.toDouble() - currentPose.heading.toDouble();
+
+        return new PoseVelocity2d(input.linearVel, Math.abs(input.angVel) * P * error);
     }
 }
